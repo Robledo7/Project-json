9d991a7080e94da59dbce937ada3c6d086307a34
Robledo7 <lurobledo.scl@gmail.com>
2024-10-21 12:31:05 -0300

    Tarjetas con archivo Json y JS y CSS

---
+++ js/main.js
@@ -0,0 +1,17 @@
+fetch("./data/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        const frame = document.getElementById("loop");
+        data.forEach(element => {
+            console.log(element);
+            let newCard = document.createElement("card");
+            newCard.className = "card";
+            newCard.id = element.id;
+            newCard.innerHTML = `<h2> ${element.name} </h2><img src="${element.img}"> <p> ${element.desc} </p>`;
+            frame.appendChild(newCard);
+        });
+
+    })
+
+    .catch((error) => console.error("Error loading JSON file", error));
